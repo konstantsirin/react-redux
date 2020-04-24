5cada08531cd5d9143dab084e3d82bfa53089432
konstantsirin <37476618+konstantsirin@users.noreply.github.com>
2020-04-24 14:52:25 +0500

    Update redux-store.js

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,7 @@ import allReducers from '../reducers/index.js';
 
 let initialState = {
     commentsPage: {
-        comments: [
-/*
-            {id: 0, author: 'admin', text: 'Первый комментарий', dateTime: '2019-11-20, 15:15:00'},
-            {id: 1, author: 'not-admin', text: 'Второй комментарий', dateTime: '2019-11-20, 15:30:00'}
-*/
-        ],
+        comments: [],
         newAuthor: '',
         newText: ''
     }
